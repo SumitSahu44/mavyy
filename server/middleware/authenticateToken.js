6e6893bbfb2f2f12d2fb5a32ec9c620d68eb34b1
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2024-11-29 00:25:43 +0530

    Update authenticateToken.js

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -2,18 +2,19 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JSONWEBTOKEN;
 
 function authenticateToken(req, res, next) {
-    const token = req.headers.authorization?.split(' ')[1]; // Assuming the JWT is stored in a cookie
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
 
     if (!token) {
-        return res.status(403).json({ message: 'Authentication token is missing' });
+        return res.status(401).json({ message: 'Authentication token is missing' });
     }
 
     try {
-        const user = jwt.verify(token, JWT_SECRET);
-        req.user = user; // Attach the decoded user info to the request
-
+        const decodedUser = jwt.verify(token, JWT_SECRET);
+        req.user = decodedUser; // Attach the decoded user info to the request object
         next(); // Proceed to the next middleware or route handler
     } catch (error) {
+        console.error('JWT Verification Error:', error.message);
         return res.status(403).json({ message: 'Invalid or expired token' });
     }
 }
